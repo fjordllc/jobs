432e7d85312389affacc906ca1364aba106e91f8
teppei machida <machida@fjord.jp>
2020-05-11 00:26:21 +0900

    静的ページ追加の設定

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  include HighVoltage::StaticPage
+
+  layout :layout_for_page
+
+  private
+
+  def layout_for_page
+    case params[:id]
+    when "welcome", "pp", "tos"
+      "lp"
+    else
+      "application"
+    end
+  end
+end
